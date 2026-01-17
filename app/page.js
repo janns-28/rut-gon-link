9b18c041defcbf0662651eb96aa7680d3bb56190
Sang <87207809+janns-28@users.noreply.github.com>
2026-01-17 23:17:05 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,1 +1,8 @@
 
+export default function Home() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontFamily: 'sans-serif' }}>
+      <h1>Trang rút gọn link của tao</h1>
+    </div>
+  )
+}
