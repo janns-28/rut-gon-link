15b29409f0376f69c3e4c35468b7d09764ab6f78
Sang <87207809+janns-28@users.noreply.github.com>
2026-01-21 23:27:06 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,4 @@
 export const metadata = {
-  title: 'Chúc Mừng Năm Mới 2026',
-  description: 'Xuân Bính Ngọ - Vạn Sự Như Ý',
-}
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="vi">
-      <body>{children}</body>
-    </html>
-  )
+  title: 'Chúc Mừng Năm Mới 2026', // Sửa ở đây nè bác!
+  description: 'Mừng Xuân Bính Ngọ',
 }
