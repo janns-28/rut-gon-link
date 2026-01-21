1bb6794b24ebb03514061cff666a27fc50544ab6
Sang <87207809+janns-28@users.noreply.github.com>
2026-01-21 20:58:15 +0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh', 
+      background: '#000', 
+      color: '#fff', 
+      fontFamily: 'sans-serif' 
+    }}>
+      <h1>🤖 Hệ thống đang chạy 100% công lực</h1>
+    </div>
+  )
+}
