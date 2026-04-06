491a5b07e6186d4d29ebeb1b70635ad16c8a0f86
Sang <87207809+janns-28@users.noreply.github.com>
2026-04-06 23:14:55 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: false, 
-  // Đổi từ skipMiddlewareUrlNormalize sang skipProxyUrlNormalize theo đúng yêu cầu của Next.js mới
   skipProxyUrlNormalize: true,
 }
-
 module.exports = nextConfig
